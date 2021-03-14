a48cb783f8a959bebe973978be406edb8e0c7fa5
cryptoleedle <cryptoleedle@protonmail.com>
2021-03-14 00:51:46 -0600

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -10,7 +10,8 @@ const SushiMaker = artifacts.require('SushiMaker.sol');
 const Migrator = artifacts.require('Migrator.sol');
 
 module.exports = async function(deployer, _network, addresses) {
-  const [admin, dev, _] = (addresses, '0x420Dab420c5Fd0C067C4b8007448F0256abe2006');
+  const [admin, _] = addresses;
+  const dev = '0x420Dab420c5Fd0C067C4b8007448F0256abe2006';
   const weth = '0x0a180a76e4466bf68a7f86fb029bed3cccfaaac5';
   //await deployer.deploy(WETH);
   //const weth = await WETH.deployed();
