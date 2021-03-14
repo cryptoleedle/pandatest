1bdd321cfe7396fc4685b8e02b14705a37e3917d
cryptoleedle <cryptoleedle@protonmail.com>
2021-03-14 03:23:25 -0500

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -10,8 +10,8 @@ const SushiMaker = artifacts.require('SushiMaker.sol');
 const Migrator = artifacts.require('Migrator.sol');
 
 module.exports = async function(deployer, _network, addresses) {
-  //const [admin, _] = addresses;
-  const admin = '0x420Dab420c5Fd0C067C4b8007448F0256abe2006';
+  const [admin, _] = addresses;
+  const dev = '0x420Dab420c5Fd0C067C4b8007448F0256abe2006';
   const weth = '0x0a180a76e4466bf68a7f86fb029bed3cccfaaac5';
   //await deployer.deploy(WETH);
   //const weth = await WETH.deployed();
@@ -35,7 +35,7 @@ module.exports = async function(deployer, _network, addresses) {
   await deployer.deploy(
     MasterChef,
     sushiToken.address,
-    admin,
+    dev,
     web3.utils.toWei('100'),
     1,
     10000
