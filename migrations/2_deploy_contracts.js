841a4d3ef17305637615c4ac9723b94fd4b619d9
cryptoleedle <cryptoleedle@protonmail.com>
2021-03-14 00:00:56 -0600

    test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,4 @@
+const UniswapV2Library = artifacts.require('uniswapv2/UniswapV2Library.sol');
 const Factory = artifacts.require('uniswapv2/UniswapV2Factory.sol');
 const Router = artifacts.require('uniswapv2/UniswapV2Router02.sol');
 const WETH = artifacts.require('WETH.sol');
@@ -9,19 +10,23 @@ const SushiMaker = artifacts.require('SushiMaker.sol');
 const Migrator = artifacts.require('Migrator.sol');
 
 module.exports = async function(deployer, _network, addresses) {
-  const [admin, _] = addresses;
-
-  await deployer.deploy(WETH);
-  const weth = await WETH.deployed();
-  const tokenA = await MockERC20.new('Token A', 'TKA', web3.utils.toWei('1000'));
-  const tokenB = await MockERC20.new('Token B', 'TKB', web3.utils.toWei('1000'));
-
+  const [admin, dev, _] = (addresses, '0x420Dab420c5Fd0C067C4b8007448F0256abe2006');
+  const weth = '0x0a180a76e4466bf68a7f86fb029bed3cccfaaac5';
+  //await deployer.deploy(WETH);
+  //const weth = await WETH.deployed();
+  //const tokenA = await MockERC20.new('Token A', 'TKA', web3.utils.toWei('1000'));
+  //const tokenB = await MockERC20.new('Token B', 'TKB', web3.utils.toWei('1000'));
+  //console.log(tokenA);
+  //console.log(tokenB);
+  
   await deployer.deploy(Factory, admin);
   const factory = await Factory.deployed();
-  await factory.createPair(weth.address, tokenA.address);
-  await factory.createPair(weth.address, tokenB.address);
-  await deployer.deploy(Router, factory.address, weth.address);
+  //await factory.createPair(tokenA.address, tokenB.address);
+  await deployer.deploy(Router, factory.address, weth);
   const router = await Router.deployed();
+
+  //await tokenA.approve(router.address, web3.utils.toWei('1000'));
+  //await tokenB.approve(router.address, web3.utils.toWei('1000'));
 
   await deployer.deploy(SushiToken);
   const sushiToken = await SushiToken.deployed();
@@ -29,12 +34,13 @@ module.exports = async function(deployer, _network, addresses) {
   await deployer.deploy(
     MasterChef,
     sushiToken.address,
-    admin,
+    dev,
     web3.utils.toWei('100'),
     1,
-    1
+    10000
   );
   const masterChef = await MasterChef.deployed();
+  await masterChef.transferOwnership(dev);
   await sushiToken.transferOwnership(masterChef.address);
 
   await deployer.deploy(SushiBar, sushiToken.address);
@@ -45,16 +51,22 @@ module.exports = async function(deployer, _network, addresses) {
     factory.address, 
     sushiBar.address, 
     sushiToken.address, 
-    weth.address
+    weth
   );
   const sushiMaker = await SushiMaker.deployed();
   await factory.setFeeTo(sushiMaker.address);
 
-  await deployer.deploy(
-    Migrator,
-    masterChef.address,
-    '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
-    factory.address,
-    1
-  );
+  //await router.addLiquidity(tokenA.address, tokenB.address, web3.utils.toWei('900'), web3.utils.toWei('900'), web3.utils.toWei('800'), web3.utils.toWei('800'), admin, 1715698160);
+  
+  //const lpaddress = await UniswapV2Library.pairFor(factory.address, tokenA.address, tokenB.address);
+  //await masterChef.add(0, lpaddress, 1);
+  //await masterChef.deposit(0, web3.utils.toWei('900'));
+
+  // await deployer.deploy(
+  //   Migrator,
+  //   masterChef.address,
+  //   '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
+  //   factory.address,
+  //   1
+  // );
 };
